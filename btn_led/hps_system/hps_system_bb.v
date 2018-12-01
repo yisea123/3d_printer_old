a0b0b0d7342ceefd7d16b644f2d4cdfddf0f6314
Spieler <volodya12309@gmail.com>
2018-12-01 07:34:50 +0300

    first hps+fpga

---
+++ btn_led/hps_system/hps_system_bb.v
@@ -0,0 +1,56 @@
+
+module hps_system (
+	clk_clk,
+	hps_io_hps_io_sdio_inst_CMD,
+	hps_io_hps_io_sdio_inst_D0,
+	hps_io_hps_io_sdio_inst_D1,
+	hps_io_hps_io_sdio_inst_CLK,
+	hps_io_hps_io_sdio_inst_D2,
+	hps_io_hps_io_sdio_inst_D3,
+	keys_external_connection_export,
+	leds_external_connection_export,
+	memory_mem_a,
+	memory_mem_ba,
+	memory_mem_ck,
+	memory_mem_ck_n,
+	memory_mem_cke,
+	memory_mem_cs_n,
+	memory_mem_ras_n,
+	memory_mem_cas_n,
+	memory_mem_we_n,
+	memory_mem_reset_n,
+	memory_mem_dq,
+	memory_mem_dqs,
+	memory_mem_dqs_n,
+	memory_mem_odt,
+	memory_mem_dm,
+	memory_oct_rzqin,
+	switches_external_connection_export);	
+
+	input		clk_clk;
+	inout		hps_io_hps_io_sdio_inst_CMD;
+	inout		hps_io_hps_io_sdio_inst_D0;
+	inout		hps_io_hps_io_sdio_inst_D1;
+	output		hps_io_hps_io_sdio_inst_CLK;
+	inout		hps_io_hps_io_sdio_inst_D2;
+	inout		hps_io_hps_io_sdio_inst_D3;
+	input	[1:0]	keys_external_connection_export;
+	output	[7:0]	leds_external_connection_export;
+	output	[14:0]	memory_mem_a;
+	output	[2:0]	memory_mem_ba;
+	output		memory_mem_ck;
+	output		memory_mem_ck_n;
+	output		memory_mem_cke;
+	output		memory_mem_cs_n;
+	output		memory_mem_ras_n;
+	output		memory_mem_cas_n;
+	output		memory_mem_we_n;
+	output		memory_mem_reset_n;
+	inout	[31:0]	memory_mem_dq;
+	inout	[3:0]	memory_mem_dqs;
+	inout	[3:0]	memory_mem_dqs_n;
+	output		memory_mem_odt;
+	output	[3:0]	memory_mem_dm;
+	input		memory_oct_rzqin;
+	input	[3:0]	switches_external_connection_export;
+endmodule
