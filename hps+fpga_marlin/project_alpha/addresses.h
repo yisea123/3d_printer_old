ab5d2ddd7b1b9d55f56410a7ed96feae6420e007
Spieler <volodya12309@gmail.com>
2018-12-29 09:38:35 +0300

    29.12.2018-2

--- hps+fpga_marlin/project_alpha/addresses.h
+++ hps+fpga_marlin/project_alpha/addresses.h
@@ -1,9 +1,18 @@
+
+
 class addresses{
 
 public:
+	addresses();
+	false init();
+	void* get_stepperX();
+	void* get_leds();
 
 protected:
 
 private:
+	void* virtual_base;
+	void* addr_stepperX;
+	void* leds;
 
 }
