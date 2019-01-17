812d7b59f958d3a64a076ac5792a5474afef96a9
Spieler <volodya12309@gmail.com>
2019-01-17 22:24:12 +0300

    17.01.2019

--- example/clk_gen.v
+++ example/clk_gen.v
@@ -1,114 +1,57 @@
-module clk_gen(
-	input 	wire 				clk, 
-	input		wire	[31:0]	reduction,
-	input 	wire 	[30:0] 	count,
-	output 	wire 				clk_out);
-	
-	reg [31:0] 	m		 = {32{1'b0}};
+`timescale 1 ps / 1 ps
+ module clk_gen(
+    input   wire                clk, 
+    input       wire    [31:0]  reduction,
+    input   wire    [30:0]  count,
+    input   wire                reset,
+    output  wire                clk_out,
+    output  reg                 finish);
+    
+    reg [31:0]  m        = {32{1'b0}};
 
-	reg 			signal = 1'b1;
-	
-	reg [31:0] 	n		 = {32{1'b0}};
-	
-	reg 			check	 = 1'b1;
-
-	reg Clk_r;
-	reg [32:0] red_r;
-	reg [31:0] count_r;
-	parameter period = 20;
-
-	initial 
-begin
-	red_r = 2;
-end
-
-	initial 
-begin
-	count_r = 3;
-end
-	
-initial
-  begin
-    Clk_r = 1'b0;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r;
-    #(period/2) Clk_r = ~Clk_r; 
-  end
-
-	always @(clk)
-	begin
-		if (check)
-		begin
-			n = count + count + 1;
-			check = 1'b0;
-		end
-		if (n!=0)
-		begin
-			if (m!=0)
-				m = m - 1;
-			else
-				begin
-					signal = ~signal;
-					m = reduction - 1;
-					n = n - 1;
-				end
-		end
-		else
-		begin
-			signal = 1'b0;
-		end
-	end
-
-	assign count = count_r;
-	assign reduction = red_r;
-	assign clk = Clk_r;
-	assign clk_out = signal;
-	
+    reg             signal = 1'b1;
+    
+    reg [31:0]  n        = {32{1'b0}};
+    
+    reg             check    = 1'b1;
+    
+    
+    always @(posedge clk or posedge reset)
+    begin
+        if (reset)
+        begin
+            finish  = 1'b0;
+            check       = 1'b1;
+            signal  = 1'b1;
+        end
+        else
+        begin
+            if (check == 1'b1)
+            begin
+                n = count + count + 1;
+                check = 1'b0;
+                finish  = 1'b0;
+            end
+            
+            if (n != 1'b0)
+            begin
+                if (m != 1'b0)
+                    m = m - 1;
+                else
+                    begin
+                        signal = ~signal;
+                        m = reduction - 1;
+                        n = n - 1;
+                    end
+            end
+            else
+            begin
+                signal = 1'b0;
+                finish = 1'b1;
+            end
+        end
+    end
+    
+    assign clk_out = signal;
+    
 endmodule
