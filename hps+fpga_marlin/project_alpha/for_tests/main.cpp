9109254627542ec6b962f8a85bcee369f2aa6c2f
Ishkhan Balgudanian <ish1999@mail.ru>
2019-04-15 21:50:10 +0300

    15.04.

--- hps+fpga_marlin/project_alpha/for_tests/main.cpp
+++ hps+fpga_marlin/project_alpha/for_tests/main.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include "addresses.h"
 #include "position.h"
 #include "gcode.h"
@@ -42,38 +42,12 @@ int main()
 	e.is_used = true;
 	speed.is_used = true;
 
-	x.num = 10;
-	y.num = 10;
-	z.num = 10;
-	e.num = 10;
-	speed.num = 500000;
-	gc.gcode_G1(x, y, z, e, speed);/*
-	//x, y, z, e, speed
-	/*
-	cout << pos.get_pos_type_driving() << endl;
-	gc.gcode_G90();
-	cout << "abs " << pos.get_pos_type_driving() << endl;
-	gc.gcode_G91();
-	cout << "rel " << pos.get_pos_type_driving() << endl;*/
-	//scanf("%" SCNx32, &speednum); //uint32_t
-	//scanf("%" SCNd32, &stepsnum); //int32_t*/
-
-	/*cout << pos.get_pos_x() << endl << pos.get_pos_y() << endl << pos.get_pos_z() << endl << pos.get_pos_e() << endl;
-	x.num = 12;
-	y.num = 14;
-	z.num = 15;
-	e.num = 16;
-	gc.gcode_G92(x,y,z,e);
-	cout << pos.get_pos_x() << endl << pos.get_pos_y() << endl << pos.get_pos_z() << endl << pos.get_pos_e() << endl;*/
-/*
-	cout << "extr pos: " << pos.get_pos_type_extruders() << endl;
-	gc.gcode_M82();
-	cout << "abs " << pos.get_pos_type_extruders() << endl;
-	gc.gcode_M83();
-	cout << "rel " << pos.get_pos_type_extruders() << endl;*/
-
-	addr.set_flags_in_start_driving_state(false);
-	
+	x.num = 1000;
+	y.num = 1000;
+	z.num = 1000;
+	e.num = 1000;
+	speed.num = 5000;
+        gc.gcode_M140(100);
 	printf("\nfinish\n");
 
 
