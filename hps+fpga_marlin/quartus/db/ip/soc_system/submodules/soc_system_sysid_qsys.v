ed84dbb2954cc352d20c183d7997af56ab2834e1
Spieler <volodya12309@gmail.com>
2019-05-06 21:01:57 +0300

    fix some bugs. recompile project.

--- hps+fpga_marlin/quartus/db/ip/soc_system/submodules/soc_system_sysid_qsys.v
+++ hps+fpga_marlin/quartus/db/ip/soc_system/submodules/soc_system_sysid_qsys.v
@@ -49,7 +49,7 @@ module soc_system_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1556900419 : 4294906626;
+  assign readdata = address ? 1557163273 : 4294906626;
 
 endmodule
 
